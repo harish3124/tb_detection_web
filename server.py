e33cca8357498627ab233e17803964cc8dab42da
Harish <dev.harish3124@gmail.com>
2023-09-09 17:03:07 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+# Path for main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory('client/public', 'index.html')
+
+# Path for all the static files
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('client/public', path)
+
+if __name__ == "__main__":
+    app.run()
